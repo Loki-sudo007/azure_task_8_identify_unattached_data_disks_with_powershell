21f0ad7f48c7368cd602eac83aae6ea590ef0830
Vitalii <stalchenkovitaliy@gmail.com>
2026-08-18 22:20:14 +0400

    identify unattached data dik

--- task.ps1
+++ task.ps1
@@ -1,1 +1,9 @@
-# Write your code here
+$resourceGroup = "mate-azure-task-5"
+
+$disks = Get-AzDisk -ResourceGroupName $resourceGroup
+
+$unattachedDisks = $disks | Where-Object {
+    $_.DiskState -eq "Unattached"
+}
+
+$unattachedDisks | ConvertTo-Json -Depth 10 | Out-File "result.json"
